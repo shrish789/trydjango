a4b4fe0e90b60986783036491d663c6ca2a1cd9a
Shrish Sharma <shrish.sharma@ril.com>
2022-05-15 23:44:29 +0530

    Chaange model without removing database

--- src/products/models.py
+++ src/products/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-	title = models.TextField()
-	description = models.TextField()
-	price = models.TextField()
-	summary = models.TextField(default = 'this is cool!')
+	title = models.CharField(max_length = 120)
+	description = models.TextField(blank = True, null = True)
+	price = models.DecimalField(decimal_places = 2, max_digits = 10000)
+	summary = models.TextField(blank = False, null = False)
+	featured = models.BooleanField() # If we want to add a column in our model then we have to give a default value to it or set null as true
