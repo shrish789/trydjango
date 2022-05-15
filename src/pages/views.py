b661fe25d3897be9f53906c984b5ecd2dfec3b37
Shrish Sharma <shrish.sharma@ril.com>
2022-05-16 00:26:30 +0530

    Djangi templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,10 +5,11 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
 	print(args, kwargs)
 	print(request.user)
-	return HttpResponse("<h1>Hello World</h1>") # String of HTML code
+	# return HttpResponse("<h1>Hello World</h1>") # String of HTML code
+	return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Contact Page</h1>")
+	return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-	return HttpResponse("<h1>About Page</h1>")
+	return render(request, "about.html", {})
