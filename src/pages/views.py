fac1aa3b7341507405adb5f21712174760cafff5
Shrish Sharma <shrish.sharma@ril.com>
2022-05-16 00:10:51 +0530

    URL routing

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,5 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
 	return HttpResponse("<h1>Hello World</h1>") # String of HTML code
+
+def contact_view(request, *args, **kwargs):
+	return HttpResponse("<h1>Contact Page</h1>")
+
+def about_view(request, *args, **kwargs):
+	return HttpResponse("<h1>About Page</h1>")
