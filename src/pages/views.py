74308cb293831e39aab57e4e71056b2adbe334e9
Shrish Sharma <shrish.sharma@ril.com>
2022-05-16 10:25:20 +0530

    Rendering contexts

--- src/pages/views.py
+++ src/pages/views.py
@@ -12,4 +12,9 @@ def contact_view(request, *args, **kwargs):
 	return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-	return render(request, "about.html", {})
+	my_context = {
+		"my_text": "This is about us",
+		"my_number": 123,
+		"my_list": [1, 2, 3, 4, 5]
+	}
+	return render(request, "about.html", my_context)
