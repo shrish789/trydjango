d22954b0096ce8db2f103b41f6db2be99e32f0f1
Shrish Sharma <shrish.sharma@ril.com>
2022-05-16 14:44:52 +0530

    HTML forms

--- src/products/views.py
+++ src/products/views.py
@@ -4,16 +4,27 @@ from .forms import ProductForm
 from .models import Product
 # Create your views here.
 
-def product_create_view(request):
-	form = ProductForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-		form = ProductForm()
+# def product_create_view(request):
+# 	form = ProductForm(request.POST or None)
+# 	if form.is_valid():
+# 		form.save()
+# 		form = ProductForm()
 
-	context = {
-		'form': form
-	}
+# 	context = {
+# 		'form': form
+# 	}
+# 	return render(request, "product/product_create.html", context)
+
+def product_create_view(request):
+	# print(request.GET)
+	# print(request.POST)
+	if request.method == "POST":
+		my_new_title = request.POST.get('title')
+		print(my_new_title)
+		# Product.objects.create(title = my_new_title)
+	context = {}
 	return render(request, "product/product_create.html", context)
+
 
 def product_detail_view(request):
 	obj = Product.objects.get(id = 1)
