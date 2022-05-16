3656ba98b42deeecf89e42343a5c0af3f887f5ad
Shrish Sharma <shrish.sharma@ril.com>
2022-05-16 17:43:13 +0530

    Form widgets

--- src/products/forms.py
+++ src/products/forms.py
@@ -13,6 +13,16 @@ class ProductForm(forms.ModelForm):
 
 class RawProductForm(forms.Form):
 	title = forms.CharField()
-	description = forms.CharField()
+	description = forms.CharField(
+		required = False,
+		widget = forms.Textarea(
+				attrs = {
+					"class": "new-class-name two",
+					"rows": 20,
+					"cols": 120,
+					"placeholder": "Description"
+				}
+			)
+		)
 	price = forms.DecimalField()
 		
